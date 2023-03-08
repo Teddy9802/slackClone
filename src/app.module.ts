c689213a4f8e2b663cc0c004d898a8a30d1622d0
Teddy <teddy@Teddyui-MacBookAir.local>
2023-03-08 17:30:08 +0900

    0308/컨트롤러 작업

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ChannelsModule } from './channels/channels.module';
+import { DmsModule } from './dms/dms.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })], //forRoot 같은 것들이 붙는 친구들은 뒤에 설정을 넣어주기 위함.
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, WorkspacesModule, ChannelsModule, DmsModule], //forRoot 같은 것들이 붙는 친구들은 뒤에 설정을 넣어주기 위함.
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
---
+++ src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('api/users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  getUsers(@Req() req) {
+    return req.user;
+  }
+
+  @Post()
+  postUsers(@Body() body: JoinRequestDto) {
+    this.usersService.postUsers(body.email, body.nickname, body.password);
+  }
+
+  @Post('login')
+  logIn(@Req() req) {
+    return req.user;
+  }
+
+  @Post('logout')
+  logOut(@Req() req, @Res() res) {
+    req.logout();
+    req.clearCookie('connect.sid', { httpOnly: true });
+    res.send('ok');
+  }
+}
