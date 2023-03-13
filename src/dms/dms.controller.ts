1b79759d6fce4816b36a59ca9c427389d4946630
Teddy <teddy@Teddyui-MacBookAir.local>
2023-03-13 19:09:03 +0900

    0313/엔티티 생성

--- src/dms/dms.controller.ts
+++ src/dms/dms.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('DM')
